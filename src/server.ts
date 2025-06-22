0528fe055f21d25007abcca10b7a622a87f4012e
md-suhag <abdussalamsuhag104@gmail.com>
2025-06-22 22:15:07 +0600

    update: env config

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ let server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
+    console.log(config.database_url);
     console.log("Connected to MongoDB successfully");
     server = app.listen(config.port, () => {
       console.log(`Server is running on port ${config.port}`);
